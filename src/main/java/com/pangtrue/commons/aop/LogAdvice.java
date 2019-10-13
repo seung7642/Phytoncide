1c3bb7707499e4033e0698d96ee6375cb6595ae2
SEUNGHO <seung7642@gmail.com>
2019-10-13 16:23:52 +0900

    AOP - LogAdvice.java

---
+++ src/main/java/com/pangtrue/commons/aop/LogAdvice.java
@@ -0,0 +1,51 @@
+package com.pangtrue.commons.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LogAdvice {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogAdvice.class);
+    
+    @Around("execution(* com.pangtrue.erp..*Controller.*(..))"
+            + " or execution(* com.pangtrue.erp..service..*Impl.*(..))"
+            + " or execution(* com.pangtrue.erp..persistence..*Impl.*(..))")
+    public Object logPrint(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object result = proceedingJoinPoint.proceed();
+
+        String type = proceedingJoinPoint.getSignature().getDeclaringTypeName();
+        String name = "";
+
+        if (type.contains("Controller")) {
+            name = "Controller : ";
+        } else if (type.contains("Service")) {
+            name = "Service : ";
+        } else if (type.contains("DAO")) {
+            name = "Persistence : ";
+        }
+
+        long end = System.currentTimeMillis();
+
+        logger.info(name + type + "."+proceedingJoinPoint.getSignature().getName() + "()");
+        logger.info("Argument/Parameter : " + Arrays.toString(proceedingJoinPoint.getArgs()));
+        if (result != null) {
+            logger.info("Return Value : " + result.toString());
+        } else {
+            logger.info("Return Type : void");
+        }
+        logger.info("Running Time : " + (end-start));
+        logger.info("----------------------------------------------------------------");
+
+        return result;
+    }
+}
