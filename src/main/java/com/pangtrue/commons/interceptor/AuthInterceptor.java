4829d3e18e20b1bf30b4fb38fecd9f5df56f5ad0
SEUNGHO <seung7642@gmail.com>
2019-11-01 01:41:08 +0900

    commons/interceptor/AuthInterceptor.java

--- src/main/java/com/pangtrue/commons/interceptor/AuthInterceptor.java
+++ src/main/java/com/pangtrue/commons/interceptor/AuthInterceptor.java
@@ -1,10 +1,17 @@
 package com.pangtrue.commons.interceptor;
 
-import javax.inject.Inject;
-
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.util.WebUtils;
 
+import javax.inject.Inject;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.pangtrue.user.model.UserVO;
 import com.pangtrue.user.service.UserService;
 
 public class AuthInterceptor extends HandlerInterceptorAdapter {
@@ -17,5 +24,44 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
         this.userService = userService;
     }
     
-    
+    // 페이지 요청 정보 저장
+    private void saveDestination(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        String query = request.getQueryString();
+        if (query == null || query.equals("null")) {
+            query = "";
+        } else {
+            query = "?" + query;
+        }
+
+        if (request.getMethod().equals("GET")) {
+            logger.info("destination : " + (uri + query));
+            request.getSession().setAttribute("destination", uri + query);
+        }
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        HttpSession httpSession = request.getSession();
+
+        if (httpSession.getAttribute("login") == null) {
+            logger.info("current user is not logged");
+            saveDestination(request);
+
+            Cookie loginCookie = WebUtils.getCookie(request, "loginCookie");
+            if (loginCookie != null) {
+                UserVO userVO = userService.checkLoginBefore(loginCookie.getValue());
+                if (userVO != null) {
+                    httpSession.setAttribute("login", userVO);
+                    return true;
+                }
+            }
+
+            response.sendRedirect("/user/login");
+            return false;
+        }
+
+        return true;
+    }
 }
