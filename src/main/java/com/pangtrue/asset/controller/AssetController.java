f0342f46c8c220a300e18b834ddee14f30041e0f
SEUNGHO <seung7642@gmail.com>
2019-12-15 22:07:55 +0900

    AssetController - GET, POST 매핑 추가

--- src/main/java/com/pangtrue/asset/controller/AssetController.java
+++ src/main/java/com/pangtrue/asset/controller/AssetController.java
@@ -1,5 +1,39 @@
 package com.pangtrue.asset.controller;
 
-public class AssetController {
+import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.pangtrue.asset.model.AssetVO;
+import com.pangtrue.asset.service.AssetService;
+
+@Controller
+public class AssetController {
+    
+    private static final Logger logger = LoggerFactory.getLogger(AssetController.class);
+    
+    @Inject
+    private AssetService assetService;
+
+    public AssetController(AssetService assetService) {
+        this.assetService = assetService;
+    }
+    
+    @GetMapping("/asset")
+    public String assetGET() throws Exception {
+        logger.info("assetGET() 호출됨");
+        return "asset/asset";
+    }
+    
+    @PostMapping("/asset")
+    public String assetPOST(AssetVO assetVO) throws Exception {
+        logger.info("assetPOST 호출됨");
+        assetService.register(assetVO);
+        
+        return "redirect:/asset/asset";
+    }
 }
